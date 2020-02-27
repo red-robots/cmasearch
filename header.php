56521fb1eb0be3e9cbf5ceb41399334dc003831d
hermie <hermiebarit@gmail.com>
2020-02-28 00:23:40 +0800

    Removing google map key in header file

--- header.php
+++ header.php
@@ -21,7 +21,7 @@
 		<link rel="profile" href="https://gmpg.org/xfn/11">
 
 		<?php wp_head(); ?>
-		<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBW8ieE1YvFCvk792K2rPfErHL6ALcb9lU"></script>
+		
 
 	</head>
 
